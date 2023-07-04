5422a16c7f27d40048adae68c02bdb8f243888a7
Cho-D-YoungRae <yrc9229@gmail.com>
2023-07-05 00:55:08 +0900

    Project Init

---
+++ src/main/java/org/cmc/curtaincallserver/CurtainCallServerApplication.java
@@ -0,0 +1,13 @@
+package org.cmc.curtaincallserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurtainCallServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurtainCallServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/cmc/curtaincallserver/CurtainCallServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cmc.curtaincallserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurtainCallServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
