74dfb97e36ed0b4f6a66a20ef73b5e4715606d8a
조영래 <yrc9229@gmail.com>
2023-07-22 01:54:11 +0900

    feat: 공연 목록 조회 API

---
+++ web/src/main/java/org/cmc/curtaincall/web/controller/ShowController.java
@@ -0,0 +1,24 @@
+package org.cmc.curtaincall.web.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.cmc.curtaincall.web.service.kopis.KopisService;
+import org.cmc.curtaincall.web.service.kopis.request.ShowListRequest;
+import org.cmc.curtaincall.web.service.kopis.response.ShowResponse;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class ShowController {
+
+    private final KopisService kopisService;
+
+    @GetMapping("/shows")
+    public Slice<ShowResponse> getShows(@Validated @ModelAttribute ShowListRequest showListRequest, Pageable pageable) {
+        return kopisService.getShows(showListRequest, pageable);
+    }
+}
