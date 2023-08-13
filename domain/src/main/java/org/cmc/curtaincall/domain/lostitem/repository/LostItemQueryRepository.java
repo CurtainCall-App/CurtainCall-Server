d8768daa8714afc8265aa5427da3c53bc01aa754
조영래 <yrc9229@gmail.com>
2023-08-13 13:30:14 +0900

    style: LostItemQueryRepository 인터페이스 삭제

--- domain/src/main/java/org/cmc/curtaincall/domain/lostitem/repository/LostItemQueryRepository.java
+++ domain/src/main/java/org/cmc/curtaincall/domain/lostitem/repository/LostItemQueryRepository.java
@@ -1,11 +1,67 @@
 package org.cmc.curtaincall.domain.lostitem.repository;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
 import org.cmc.curtaincall.domain.lostitem.LostItem;
+import org.cmc.curtaincall.domain.lostitem.LostItemType;
 import org.cmc.curtaincall.domain.lostitem.request.LostItemQueryParam;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
+import org.springframework.stereotype.Repository;
 
-public interface LostItemQueryRepository {
+import java.util.List;
+import java.util.Optional;
 
-    Slice<LostItem> query(Pageable pageable, LostItemQueryParam queryParam);
+import static org.cmc.curtaincall.domain.lostitem.QLostItem.lostItem;
+
+@Repository
+@RequiredArgsConstructor
+public class LostItemQueryRepository {
+
+    private final JPAQueryFactory query;
+
+    public Slice<LostItem> query(Pageable pageable, LostItemQueryParam queryParam) {
+        List<LostItem> content = query
+                .selectFrom(lostItem)
+                .join(lostItem.facility).fetchJoin()
+                .join(lostItem.image).fetchJoin()
+                .where(
+                        titleStartsWith(queryParam.getTitle()),
+                        facilityIdEq(queryParam.getFacilityId()),
+                        typeEq(queryParam.getType()),
+                        lostItem.useYn.isTrue()
+                )
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize() + 1L)
+                .fetch();
+
+        boolean hasNext = false;
+        if (content.size() > pageable.getPageSize()) {
+            content.remove(pageable.getPageSize());
+            hasNext = true;
+        }
+
+        return new SliceImpl<>(content, pageable, hasNext);
+    }
+
+    private BooleanExpression titleStartsWith(String title) {
+        return Optional.ofNullable(title)
+                .map(lostItem.title::startsWith)
+                .orElse(null);
+    }
+
+    private BooleanExpression facilityIdEq(String facilityId) {
+        return Optional.ofNullable(facilityId)
+                .map(lostItem.facility.id::eq)
+                .orElse(null);
+    }
+
+    private BooleanExpression typeEq(LostItemType type) {
+        return Optional.ofNullable(type)
+                .map(lostItem.type::eq)
+                .orElse(null);
+    }
+
 }
