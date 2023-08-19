f878a09cdb718b7f6f6d6ee6b27d2390ff1ce803
조영래 <yrc9229@gmail.com>
2023-08-19 17:04:36 +0900

    feat: Batch 세팅

---
+++ batch/src/main/java/org/cmc/curtaincall/batch/BatchApplication.java
@@ -0,0 +1,9 @@
+package org.cmc.curtaincall.batch;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableBatchProcessing
+public class BatchApplication {
+}
