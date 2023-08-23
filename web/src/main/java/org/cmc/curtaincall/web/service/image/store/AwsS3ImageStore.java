ed131c4407b08128927238c6fcb18669939c4ff3
조영래 <yrc9229@gmail.com>
2023-08-24 00:53:50 +0900

    fix: aws s3 버전 업데이트 대응

--- web/src/main/java/org/cmc/curtaincall/web/service/image/store/AwsS3ImageStore.java
+++ web/src/main/java/org/cmc/curtaincall/web/service/image/store/AwsS3ImageStore.java
@@ -1,18 +1,15 @@
 package org.cmc.curtaincall.web.service.image.store;
 
-import com.amazonaws.auth.InstanceProfileCredentialsProvider;
-import com.amazonaws.regions.Regions;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.CannedAccessControlList;
-import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.amazonaws.services.s3.model.PutObjectRequest;
+import io.awspring.cloud.s3.ObjectMetadata;
+import io.awspring.cloud.s3.S3Resource;
+import io.awspring.cloud.s3.S3Template;
 import org.cmc.curtaincall.web.exception.InvalidImageException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
+import software.amazon.awssdk.services.s3.S3Client;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -27,16 +24,15 @@ public class AwsS3ImageStore implements ImageStore {
 
     private final String bucketName;
 
-    private final AmazonS3 s3Client;
+    private final S3Client s3Client;
+
+    private final S3Template s3Template;
 
     public AwsS3ImageStore(
-            @Value("${cloud.aws.s3.bucket}") String bucketName) {
+            @Value("${app.s3.bucket-name}") String bucketName, S3Client s3Client, S3Template s3Template) {
         this.bucketName = bucketName;
-        InstanceProfileCredentialsProvider credentialsProvider = InstanceProfileCredentialsProvider.getInstance();
-        this.s3Client = AmazonS3ClientBuilder.standard()
-                .withRegion(Regions.AP_NORTHEAST_1)
-                .withCredentials(credentialsProvider)
-                .build();
+        this.s3Client = s3Client;
+        this.s3Template = s3Template;
     }
 
     public String store(Resource image, String storedName) {
@@ -44,12 +40,11 @@ public class AwsS3ImageStore implements ImageStore {
             String filename = Optional.ofNullable(image.getFilename())
                     .orElseThrow(() -> new InvalidImageException("filename=" + image.getFilename()));
             String contentType = Files.probeContentType(Path.of(filename));
-            ObjectMetadata metadata = new ObjectMetadata();
-            metadata.setContentLength(image.contentLength());
-            metadata.setContentType(contentType);
-            s3Client.putObject(new PutObjectRequest(bucketName, storedName, input, metadata)
-                    .withCannedAcl(CannedAccessControlList.PublicRead));
-            return s3Client.getUrl(bucketName, storedName).toString();
+            ObjectMetadata metadata = ObjectMetadata.builder()
+                    .contentType(contentType)
+                    .build();
+            S3Resource resource = s3Template.upload(bucketName, storedName, input, metadata);
+            return resource.getURL().toString();
         } catch (IOException e) {
             throw new InvalidImageException(e);
         }
