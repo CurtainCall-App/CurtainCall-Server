0610c11b28d8cb8e374d10977dd2c07964977e5d
조영래 <yrc9229@gmail.com>
2023-08-25 10:52:07 +0900

    fix: 파티 상세 조회 누락된 category 추가

--- web/src/main/java/org/cmc/curtaincall/web/service/party/response/PartyDetailResponse.java
+++ web/src/main/java/org/cmc/curtaincall/web/service/party/response/PartyDetailResponse.java
@@ -2,9 +2,12 @@ package org.cmc.curtaincall.web.service.party.response;
 
 
 import lombok.*;
+import org.cmc.curtaincall.domain.image.Image;
+import org.cmc.curtaincall.domain.party.Party;
 import org.cmc.curtaincall.domain.party.PartyCategory;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Getter
 @Builder
@@ -42,4 +45,26 @@ public class PartyDetailResponse {
 
     private String facilityName;
 
+    public static PartyDetailResponse of(Party party) {
+        return PartyDetailResponse.builder()
+                .id(party.getId())
+                .title(party.getTitle())
+                .content(party.getContent())
+                .category(party.getCategory())
+                .curMemberNum(party.getCurMemberNum())
+                .maxMemberNum(party.getMaxMemberNum())
+                .showAt(party.getShowAt())
+                .createdAt(party.getCreatedAt())
+                .creatorId(party.getCreatedBy().getId())
+                .creatorNickname(party.getCreatedBy().getNickname())
+                .creatorImageUrl(Optional.ofNullable(party.getCreatedBy().getImage())
+                        .filter(Image::getUseYn)
+                        .map(Image::getUrl)
+                        .orElse(null))
+                .showId(party.getShow().getId())
+                .showName(party.getShow().getName())
+                .facilityId(party.getShow().getFacility().getId())
+                .facilityName(party.getShow().getFacility().getName())
+                .build();
+    }
 }
