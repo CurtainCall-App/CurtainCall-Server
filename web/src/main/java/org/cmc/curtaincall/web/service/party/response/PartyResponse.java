024f4598f59b314443b00ab142da090351882a82
조영래 <yrc9229@gmail.com>
2023-08-26 14:02:49 +0900

    feat: 파티 리스트 조회 공연 null 허용

--- web/src/main/java/org/cmc/curtaincall/web/service/party/response/PartyResponse.java
+++ web/src/main/java/org/cmc/curtaincall/web/service/party/response/PartyResponse.java
@@ -5,6 +5,7 @@ import lombok.*;
 import org.cmc.curtaincall.domain.image.Image;
 import org.cmc.curtaincall.domain.party.Party;
 import org.cmc.curtaincall.domain.party.PartyCategory;
+import org.cmc.curtaincall.domain.show.Show;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
@@ -46,6 +47,7 @@ public class PartyResponse {
     private String facilityName;
 
     public static PartyResponse of(Party party) {
+        Optional<Show> showOptional = Optional.ofNullable(party.getShow());
         return PartyResponse.builder()
                 .id(party.getId())
                 .title(party.getTitle())
@@ -60,11 +62,11 @@ public class PartyResponse {
                         .filter(Image::getUseYn)
                         .map(Image::getUrl)
                         .orElse(null))
-                .showId(party.getShow().getId())
-                .showName(party.getShow().getName())
-                .showPoster(party.getShow().getPoster())
-                .facilityId(party.getShow().getFacility().getId())
-                .facilityName(party.getShow().getFacility().getName())
+                .showId(showOptional.map(Show::getId).orElse(null))
+                .showName(showOptional.map(Show::getName).orElse(null))
+                .showPoster(showOptional.map(Show::getPoster).orElse(null))
+                .facilityId(showOptional.map(show -> show.getFacility().getId()).orElse(null))
+                .facilityName(showOptional.map(show -> show.getFacility().getName()).orElse(null))
                 .build();
     }
 }
