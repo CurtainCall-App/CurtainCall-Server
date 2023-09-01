abb11d0eebfeaa8acf2461e89b0fcb91d825c301
조영래 <yrc9229@gmail.com>
2023-09-01 23:25:12 +0900

    feat: 공연장 업데이트 존재하는 데이터 필터링 추가

--- batch/src/main/java/org/cmc/curtaincall/batch/job/facility/FacilityItemProcessor.java
+++ batch/src/main/java/org/cmc/curtaincall/batch/job/facility/FacilityItemProcessor.java
@@ -1,19 +1,28 @@
 package org.cmc.curtaincall.batch.job.facility;
 
+import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.cmc.curtaincall.batch.service.kopis.KopisService;
 import org.cmc.curtaincall.batch.service.kopis.response.FacilityDetailResponse;
 import org.cmc.curtaincall.batch.service.kopis.response.FacilityResponse;
 import org.cmc.curtaincall.domain.show.Facility;
 import org.springframework.batch.item.ItemProcessor;
 
+@Slf4j
 @RequiredArgsConstructor
 public class FacilityItemProcessor implements ItemProcessor<FacilityResponse, Facility> {
 
     private final KopisService kopisService;
 
+    private final EntityManager em;
+
     @Override
     public Facility process(FacilityResponse item) throws Exception {
+        if (isFacilityExisting(item.id())) {
+            log.debug("공연시설({})은 존재하는 데이터입니다.", item.id());
+            return null;
+        }
         FacilityDetailResponse facilityDetail = kopisService.getFacilityDetail(item.id());
         return Facility.builder()
                 .id(facilityDetail.id())
@@ -30,5 +39,16 @@ public class FacilityItemProcessor implements ItemProcessor<FacilityResponse, Fa
                 .latitude(facilityDetail.latitude())
                 .longitude(facilityDetail.longitude())
                 .build();
+    }
+
+    private boolean isFacilityExisting(String id) {
+        return !em.createQuery("""
+                            select 1
+                            from Facility facility
+                            where facility.id = :id
+                        """, Integer.class)
+                .setParameter("id", id)
+                .getResultList()
+                .isEmpty();
     }
 }
