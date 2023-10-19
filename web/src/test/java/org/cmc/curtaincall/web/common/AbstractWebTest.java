ec704b388b234eef7c68f6a575315c54756b6d13
Cho Youngrae <yrc9229@gmail.com>
2023-10-19 22:09:06 +0900

    test: 웹 테스트 추상 클래스 추가

---
+++ web/src/test/java/org/cmc/curtaincall/web/common/AbstractWebTest.java
@@ -0,0 +1,36 @@
+package org.cmc.curtaincall.web.common;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.cmc.curtaincall.domain.account.MemberId;
+import org.cmc.curtaincall.domain.account.dao.AccountDao;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+
+@Import(RestDocsConfig.class)
+@AutoConfigureRestDocs
+public abstract class AbstractWebTest {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    @MockBean
+    protected AccountDao accountDao;
+
+    @BeforeEach
+    void setUpAccountDao() {
+        given(accountDao.findMemberIdByUsername(any()))
+                .willReturn(Optional.of(new MemberId(1234L)));
+    }
+}
