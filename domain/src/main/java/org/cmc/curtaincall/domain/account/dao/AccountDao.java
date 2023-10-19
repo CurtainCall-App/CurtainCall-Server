1c5312e4919b08975a375b3ace64724c11b74d3f
Cho Youngrae <yrc9229@gmail.com>
2023-10-19 20:51:53 +0900

    feat: AccountDao findMemberIdByUsername 추가

---
+++ domain/src/main/java/org/cmc/curtaincall/domain/account/dao/AccountDao.java
@@ -0,0 +1,24 @@
+package org.cmc.curtaincall.domain.account.dao;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.cmc.curtaincall.domain.account.MemberId;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+import static org.cmc.curtaincall.domain.account.QAccount.account;
+
+@RequiredArgsConstructor
+@Repository
+public class AccountDao {
+
+    private final JPAQueryFactory query;
+
+    public Optional<MemberId> findMemberIdByUsername(String username) {
+        return Optional.ofNullable(query.select(account.memberId)
+                .from(account)
+                .where(account.username.eq(username))
+                .fetchOne());
+    }
+}
