0826b89ce722353819448787bf6158d9f74b1bdb
Cho Youngrae <yrc9229@gmail.com>
2023-10-19 23:51:26 +0900

    docs: 로그인 페이지

--- web/src/test/java/org/cmc/curtaincall/web/security/AuthDocsController.java
+++ web/src/test/java/org/cmc/curtaincall/web/security/AuthDocsController.java
@@ -1,19 +1,15 @@
 package org.cmc.curtaincall.web.security;
 
-import org.cmc.curtaincall.web.security.response.LoginResponse;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/docs/auth")
 @RestController
 public class AuthDocsController {
 
-    @GetMapping("/login-response")
-    public ResponseEntity<LoginResponse> loginResponse() {
-        LoginResponse loginResponse = new LoginResponse("<Access token>");
-        return ResponseEntity.ok(loginResponse);
+    @GetMapping("/oauth2/authorization/{provider}")
+    public String loginPage(@PathVariable String provider) {
+        return "loginPage";
     }
 
 }
--- web/src/test/java/org/cmc/curtaincall/web/security/AuthDocsTest.java
+++ web/src/test/java/org/cmc/curtaincall/web/security/AuthDocsTest.java
@@ -1,25 +1,27 @@
 package org.cmc.curtaincall.web.security;
 
 import org.cmc.curtaincall.web.common.AbstractWebTest;
+import org.cmc.curtaincall.web.common.RestDocsAttribute;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.security.test.context.support.WithMockUser;
 
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
-import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 
 @WebMvcTest(AuthDocsController.class)
 class AuthDocsTest extends AbstractWebTest {
 
     @Test
     @WithMockUser
-    void loginResponse() throws Exception {
-        mockMvc.perform(get("/docs/auth/login-response"))
-                .andDo(document("auth-LoginResponse",
-                        responseFields(
-                                fieldWithPath("accessToken").description("엑세스 토큰")
+    void loginPage() throws Exception {
+        mockMvc.perform(get("/oauth2/authorization/{provider}", "kakao"))
+                .andDo(document("auth-login-page",
+                        pathParameters(
+                                parameterWithName("provider").description("OAuth2 Provider")
+                                        .attributes(RestDocsAttribute.constraint("kakao"))
                         )
                 ));
     }
