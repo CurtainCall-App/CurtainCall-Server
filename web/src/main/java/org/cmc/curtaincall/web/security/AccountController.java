1f8486f4608f18526ecd6db54f868a18869eaa45
Cho Youngrae <yrc9229@gmail.com>
2023-10-20 01:35:05 +0900

    feat: 로그인 유저 회원 ID

--- web/src/main/java/org/cmc/curtaincall/web/security/AccountController.java
+++ web/src/main/java/org/cmc/curtaincall/web/security/AccountController.java
@@ -1,6 +1,9 @@
 package org.cmc.curtaincall.web.security;
 
 import lombok.RequiredArgsConstructor;
+import org.cmc.curtaincall.domain.account.dao.AccountDao;
+import org.cmc.curtaincall.domain.member.MemberId;
+import org.cmc.curtaincall.web.common.response.IdResult;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,11 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AccountController {
 
-    // TODO memberId 조회
+    private final AccountDao accountDao;
 
-    @GetMapping("/user")
-    public Authentication getUser(Authentication authentication) {
-
-        return authentication;
+    @GetMapping("/user/member-id")
+    public IdResult<Long> getUserMemberId(Authentication authentication) {
+        String username = authentication.getName();
+        return new IdResult<>(accountDao.findMemberIdByUsername(username).map(MemberId::getId).orElse(null));
     }
 }
