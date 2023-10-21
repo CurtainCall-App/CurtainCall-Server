c4d63bcf0e93201e947f7a7ab692d3e1c00c49a8
Cho Youngrae <yrc9229@gmail.com>
2023-10-22 01:36:34 +0900

    feat: ShowReviewId 추가

---
+++ domain/src/main/java/org/cmc/curtaincall/domain/review/ShowReviewId.java
@@ -0,0 +1,23 @@
+package org.cmc.curtaincall.domain.review;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.*;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Embeddable
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EqualsAndHashCode
+@ToString
+public class ShowReviewId implements Serializable {
+
+    @Column(name = "show_review_id")
+    private Long id;
+
+    public ShowReviewId(final Long id) {
+        this.id = Objects.requireNonNull(id);
+    }
+}
