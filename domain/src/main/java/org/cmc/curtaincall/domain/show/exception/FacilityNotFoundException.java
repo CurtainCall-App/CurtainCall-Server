b4ad777820270c265389ca4aec2a1d5d0cc1537a
Cho Youngrae <yrc9229@gmail.com>
2023-11-02 19:05:58 +0900

    feat: LostItemFacilityValidator

---
+++ domain/src/main/java/org/cmc/curtaincall/domain/show/exception/FacilityNotFoundException.java
@@ -0,0 +1,17 @@
+package org.cmc.curtaincall.domain.show.exception;
+
+import org.cmc.curtaincall.domain.common.DomainErrorCode;
+import org.cmc.curtaincall.domain.common.DomainException;
+import org.cmc.curtaincall.domain.show.FacilityId;
+
+public class FacilityNotFoundException extends DomainException {
+
+    public FacilityNotFoundException(final FacilityId facilityId) {
+        super(DomainErrorCode.NOT_FOUND, "FacilityId=" + facilityId);
+    }
+
+    @Override
+    public String getExternalMessage() {
+        return "존재하지 않는 공연장입니다.";
+    }
+}
