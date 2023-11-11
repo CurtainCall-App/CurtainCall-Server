d7506f7fb2374a90ffc0d48787c11da2afebfcc0
Cho Youngrae <yrc9229@gmail.com>
2023-11-11 11:17:03 +0900

    feat: ShowCreatedEvent

---
+++ domain/src/main/java/org/cmc/curtaincall/domain/show/event/ShowCreatedEvent.java
@@ -0,0 +1,21 @@
+package org.cmc.curtaincall.domain.show.event;
+
+import org.cmc.curtaincall.domain.core.AbstractDomainEvent;
+import org.cmc.curtaincall.domain.show.ShowGenre;
+import org.cmc.curtaincall.domain.show.ShowId;
+import org.cmc.curtaincall.domain.show.ShowState;
+
+public class ShowCreatedEvent extends AbstractDomainEvent<ShowCreatedEvent.Source> {
+
+    public ShowCreatedEvent(final ShowCreatedEvent.Source source) {
+        super(source);
+    }
+
+    public record Source(
+          ShowId id,
+          ShowGenre genre,
+          ShowState state
+    ) {
+    }
+
+}
