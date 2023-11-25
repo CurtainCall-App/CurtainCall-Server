6bbc6935ad3b485142e0dde54ff9cc24964b7f90
Cho Youngrae <yrc9229@gmail.com>
2023-11-26 01:43:16 +0900

    fix: LoginAuditor stackoverflow

--- domain/src/main/java/org/cmc/curtaincall/domain/account/dao/AccountDao.java
+++ domain/src/main/java/org/cmc/curtaincall/domain/account/dao/AccountDao.java
@@ -24,7 +24,6 @@ public class AccountDao {
                 .where(
                         account.username.eq(username),
                         member.useYn
-
                 )
                 .fetchOne());
     }
