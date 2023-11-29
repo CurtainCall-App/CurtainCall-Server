ee30f6b0c780d91744956b6edf460ac09a731e55
Cho Youngrae <yrc9229@gmail.com>
2023-11-29 23:44:01 +0900

    feat: 회원 도메인 예외 적용

--- domain/src/main/java/org/cmc/curtaincall/domain/member/exception/MemberNotFoundException.java
+++ domain/src/main/java/org/cmc/curtaincall/domain/member/exception/MemberNotFoundException.java
@@ -1,17 +1,11 @@
 package org.cmc.curtaincall.domain.member.exception;
 
-import org.cmc.curtaincall.domain.core.DomainErrorCode;
-import org.cmc.curtaincall.domain.core.DomainException;
+import org.cmc.curtaincall.domain.core.AbstractDomainException;
 import org.cmc.curtaincall.domain.member.MemberId;
 
-public class MemberNotFoundException extends DomainException {
+public class MemberNotFoundException extends AbstractDomainException {
 
     public MemberNotFoundException(final MemberId memberId) {
-        super(DomainErrorCode.NOT_FOUND, memberId.toString());
-    }
-
-    @Override
-    public String getExternalMessage() {
-        return "존재하지 않는 회원입니다.";
+        super(MemberErrorCode.NOT_FOUND, "memberId=" + memberId);
     }
 }
