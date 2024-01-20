83df0bb70c2a9c1bed20e6154e4c091eac0415dc
Cho Youngrae <yrc9229@gmail.com>
2024-01-20 21:41:13 +0900

    feat: 파티 삭제 개선

--- web/src/main/java/org/cmc/curtaincall/web/party/PartyDeleteService.java
+++ web/src/main/java/org/cmc/curtaincall/web/party/PartyDeleteService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.cmc.curtaincall.domain.party.Party;
 import org.cmc.curtaincall.domain.party.PartyId;
 import org.cmc.curtaincall.domain.party.exception.PartyNotFoundException;
+import org.cmc.curtaincall.domain.party.repository.PartyMemberRepository;
 import org.cmc.curtaincall.domain.party.repository.PartyRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,12 +15,13 @@ public class PartyDeleteService {
 
     private final PartyRepository partyRepository;
 
+    private final PartyMemberRepository partyMemberRepository;
+
     @Transactional
     public void delete(final PartyId partyId) {
         final Party party = partyRepository.findWithPessimisticLockById(partyId.getId())
                 .orElseThrow(() -> new PartyNotFoundException(partyId));
-        party.getPartyMembers()
-                .forEach(partyMember -> party.cancelParticipate(partyMember.getMemberId()));
+        partyMemberRepository.deleteAllByParty(party);
         partyRepository.delete(party);
     }
 }
